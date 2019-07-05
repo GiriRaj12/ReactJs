6ddd1a1864a04bdf8405f66dfb029a5737f4e1d0
Giri <girirajsekar50@gmail.com>
2019-07-05 18:35:10 +0530

    Changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,12 @@ class SideBar extends React.Component{
     render(){
         return (
             <div className="SideBar">
+                <ul className="sideList">
+                    <li className="elements"><i class="fas fa-home"></i></li>
+                    <li className="elements"><i class="fas fa-chart-line"></i></li>
+                    <li className="elements"><i class="fas fa-sliders-h"></i></li>
+                    <li className="elements"><i class="fas fa-clipboard-list"></i></li>
+                </ul>
             </div>
         );
     }
