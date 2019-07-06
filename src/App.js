35389cf01171b81b5b954d3e55164f5e640c88c6
Giri <girirajsekar50@gmail.com>
2019-07-06 15:14:16 +0530

    Changes in the profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Profile from './components/Profile';
 import SideBar from './components/Sidebar';
 
 class App extends React.Component{
  render(){
   return (
     <div className="App">
+            <div><Profile/></div>
             <div><SideBar/></div>
     </div>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './profile.css';
 
 class Profile extends React.Component{
     state ={
@@ -7,6 +8,13 @@ class Profile extends React.Component{
     render(){
         return (
             <div className="Profile">
+                <nav>
+                    <div className="logo">
+                        <div id="fastLogo"><i class="fa fa-shipping-fast"></i></div>
+                    </div>
+                    <div id="facebook"><i class="fab fa-facebook"></i></div>
+                    <div id="admin"><i class="fas fa-user-cog"></i></div>
+                </nav>
             </div>
         );
 }
