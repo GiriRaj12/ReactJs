8ddf7435d6a504ad25923c7bc82f8fcfcf213963
Giri <girirajsekar50@gmail.com>
2019-07-10 19:53:23 +0530

    Changes

--- src/components/Feeds.js
+++ src/components/Feeds.js
@@ -8,6 +8,10 @@ class Feeds extends React.Component{
                 <div className="feedsView">
                 <p id="feedParagrapgh">Feeds</p>
                </div>
+               <div className="feedContent">
+                   <input id="feedInput" type="text" placeholder="Status"></input>
+                   <button id="postButton">Post</button>
+               </div>
             </div>
         );
     }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,11 +2,15 @@ import React from 'react';
 import "./CompCSS/sideBar.css";
 
 class SideBar extends React.Component{
+    homeButton = () => {
+        console.log("class event triggered");
+        document.getElementsByClassName("SideBar").display = "none";
+    }
     render(){
         return (
-            <div className="SideBar">
+            <div className="SideBar" display ="block">
                 <ul className="sideList">
-                    <li className="elements"><i class="fas fa-home"></i></li>
+                    <li className="elements"><a id ="homeButton" onClick={this.homeButton}><i class="fas fa-home"></i></a> </li>
                     <li className="elements"><i class="fas fa-chart-line"></i></li>
                     <li className="elements"><i class="fas fa-sliders-h"></i></li>
                     <li className="elements"><i class="fas fa-clipboard-list"></i></li>
