0d05e02d42b8704a50d5b3440711100b8f67a010
Giriraj <girirajsekar50@gmail.com>
2019-08-10 21:35:39 +0530

    Changes Made

--- src/components/SubComponents/Loans.js
+++ src/components/SubComponents/Loans.js
@@ -14,8 +14,14 @@ class Loans extends React.Component{
                 <p className="bagroundTextLoan">Loans and other borrowal's</p>
                 <p className="plusSign" onClick={this.addLoans}><i class="fas fa-plus-circle"></i></p>
                 <div className="addLoans">
+                    <label>Bank Name :</label>
+                    <input className="bankName" type="text" placeholder="Bank Name"></input>
+                    <label>Loan Amount :</label>
                     <input className="loanAmount" type="Number" placeholder="Principle Amount"></input>
-                    
+                    <label>Interest </label>
+                    <input className="interestAmount" type="Number" placeholder="Interest for Principle"></input><br></br>
+                    <label>Months :</label>
+                    <input className="monthsPlanned" type="Number" min="0" placeholder="Months Planned"></input>
                 </div>
             </div>
         );
